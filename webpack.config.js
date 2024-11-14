bb4b1bdc229d32e91489c35523ff6bbe8904931f
Nabil Baugher <nabilbaugher@gmail.com>
2024-11-13 21:08:27 -0800

    make deploy?

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,26 @@
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const extractCSS = new ExtractTextPlugin('[name].fonts.css');
-const extractSCSS = new ExtractTextPlugin('[name].styles.css');
+const extractCSS = new ExtractTextPlugin("[name].fonts.css");
+const extractSCSS = new ExtractTextPlugin("[name].styles.css");
 
-const BUILD_DIR = path.resolve(__dirname, 'build');
-const SRC_DIR = path.resolve(__dirname, 'src');
+const BUILD_DIR = path.resolve(__dirname, "build");
+const SRC_DIR = path.resolve(__dirname, "src");
 
-console.log('BUILD_DIR', BUILD_DIR);
-console.log('SRC_DIR', SRC_DIR);
+console.log("BUILD_DIR", BUILD_DIR);
+console.log("SRC_DIR", SRC_DIR);
 
 module.exports = {
   entry: {
-    index: [SRC_DIR + '/index.js']
-	},
+    index: [SRC_DIR + "/index.js"],
+  },
   output: {
     path: BUILD_DIR,
-    filename: '[name].bundle.js'
+    filename: "[name].bundle.js",
+    publicPath: "./", // Add this line
   },
   watch: true,
   devServer: {
@@ -27,7 +28,7 @@ module.exports = {
     //   port: 9001,
     compress: true,
     hot: true,
-    open: true
+    open: true,
   },
   module: {
     rules: [
@@ -35,58 +36,61 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             cacheDirectory: true,
-            presets: ['react', 'env']
-          }
-        }
+            presets: ["react", "env"],
+          },
+        },
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
+        loader: "html-loader",
       },
       {
         test: /\.(scss)$/,
-        use: ['css-hot-loader'].concat(extractSCSS.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: { alias: { '../img': '../public/img' } }
-            },
-            {
-              loader: 'sass-loader'
-            }
-          ]
-        }))
+        use: ["css-hot-loader"].concat(
+          extractSCSS.extract({
+            fallback: "style-loader",
+            use: [
+              {
+                loader: "css-loader",
+                options: { alias: { "../img": "../public/img" } },
+              },
+              {
+                loader: "sass-loader",
+              },
+            ],
+          })
+        ),
       },
       {
         test: /\.css$/,
         use: extractCSS.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
+          fallback: "style-loader",
+          use: "css-loader",
+        }),
       },
       {
         test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
             // loader: 'url-loader'
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: './img/[name].[hash].[ext]'
-            }
-          }
-        ]
+              name: "./img/[name].[hash].[ext]",
+            },
+          },
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: './fonts/[name].[hash].[ext]'
-        }
-      }]
+          name: "./fonts/[name].[hash].[ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
@@ -94,17 +98,12 @@ module.exports = {
     new webpack.NamedModulesPlugin(),
     extractCSS,
     extractSCSS,
-    new HtmlWebpackPlugin(
-      {
-        inject: true,
-        template: './public/index.html'
-      }
-    ),
-    new CopyWebpackPlugin([
-        {from: './public/img', to: 'img'}
-      ],
-      {copyUnmodified: false}
-    )
-  ]
-}
-;
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+    }),
+    new CopyWebpackPlugin([{ from: "./public/img", to: "img" }], {
+      copyUnmodified: false,
+    }),
+  ],
+};
